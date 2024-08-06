cc27d81f394013088e01b58b4f11a6f22cd9cc66
Gamer4lifeovi <ukporheo@gmail.com>
2024-08-06 11:51:28 +0100

    new changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <nav className="flex w-full flex-row items-center justify-between pt-5 pl-2">
-      <div className="flex ">
-        <h1 className="text-3xl">Maverick Blog</h1>
+      <div className="flex justify-between">
         <img src="/2.png" alt=""  className="w-8"/>
+        <h1 className="text-3xl">Maverick Blog</h1>
       </div>
-      <div className="pr-5 justify-between">
+      <div className="pr-5 justify-between flex">
       <a href="#">About Us</a>
       <a href="#">Blogs</a>
       <a href="#">Pricing</a>
