df444c5b323179c3a0bcefbe42145742d048519f
Gamer4lifeovi <ukporheo@gmail.com>
2024-08-27 16:44:31 +0100

    New Commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,23 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <div className="bg-[#FFFDD0]">
-        <header>
-          <nav></nav>
+      <section className="bg-[#f7f4ed]">
+        <header className="flex">
+          <nav className="flex">
+              <div>
+                <h1 className="text-center capitalize md:uppercase">Mavrick</h1>
+              </div>
+
+              <div>
+                <a href="#">Our Story</a>
+                <a href="#">Membership</a>
+                <a href="#">Write</a>
+                <a href="#">Sign-in</a>
+                <a href="#">Register</a>
+              </div>
+          </nav>
         </header>
-      </div>
+      </section>
     </>
   )
 }
